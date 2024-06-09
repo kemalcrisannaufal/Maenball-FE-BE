0fe36131a670ff53a3901bdb12670e3651585ff2
unknown <kemalcrisannnaufal@gmail.com>
2024-06-09 14:20:15 +0700

    Fix Fixture(Users)

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -3,42 +3,24 @@
 namespace App\Http\Controllers;
 
 use GuzzleHttp\Client;
+use App\Models\Fixture;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
-    public function index(Request $request)
-{
-    $client = new Client();
-    $response = $client->request('GET', 'https://livescore-api.com/api-client/fixtures/matches.json?key=e6l0EOXMmvzA4Ckl&secret=SGWyI0VUktY9AeLPl6QZVOMijmSnYcJR&competition_id=66');
-    $data = json_decode($response->getBody(), true);
+   public function index()
+    {
+        $fixtures = Fixture::with(['homeTeam', 'awayTeam'])->paginate(6);
+        $schedule = $fixtures->toArray();
+        $main_schedule = $schedule['data'][0];
+        $schedule = array_slice($schedule['data'], 1);
 
-    $main_schedule = null;
-    $schedule = null;
 
-    if (count($data['data']['fixtures']) > 0) {
-        $main_schedule =  $data['data']['fixtures'][0];
-        $schedule = array_slice($data['data']['fixtures'], 1);
-
-        // Paginate the schedule
-        $perPage = 5; // Number of items per page
-        $page = $request->input('page', 1); // Get the current page or default to 1
-        $offset = ($page - 1) * $perPage;
-        $items = array_slice($schedule, $offset, $perPage);
-
-        $schedule = new \Illuminate\Pagination\LengthAwarePaginator(
-            $items,
-            count($schedule),
-            $perPage,
-            $page,
-            ['path' => $request->url(), 'query' => $request->query()]
-        );
+        return view('schedule.schedule', [
+            'fixtures' => $fixtures,
+            'main_schedule' => $main_schedule,
+            'schedule' => $schedule
+        ]);
     }
-
-    return view('schedule.schedule', [
-        'main_schedule' => $main_schedule,
-        'schedule' => $schedule,
-    ]);
-}
 
 }
--- resources/views/schedule/schedule.blade.php
+++ resources/views/schedule/schedule.blade.php
@@ -10,34 +10,34 @@
             <div class="col-md-10">
                 @if ($main_schedule != null)
                     @php
-                        $utcTime = $main_schedule['date'] . ' ' . $main_schedule['time'];
-                        $localTime = Carbon\Carbon::parse($utcTime)->setTimezone('Asia/Jakarta');
-                        $time = explode(' ', $localTime);
+                        $kickoff = Carbon\Carbon::parse($main_schedule['kickoff'])->format('Y-m-d H:i');
                     @endphp
                     <div class="img-box">
                         <img src="images/schedule.png" alt="" class="img">
                         <div class="newest-schedule-match">
                             <p class="fw-bold fs-2 text-white mb-3" id="title">Upcoming Match</p>
                             <div class="d-flex justify-content-start align-items-center gap-3">
-                                <p class="fw-bold fs-4">{{ $main_schedule['home_name'] }}</p>
+                                <img src="{{ '/storage/logo/' . $main_schedule['home_team']['logo'] }}" alt=""
+                                    width="80px">
                                 <p class="fw-bold fs-2 text-white">VS</p>
-                                <p class="fw-bold fs-4">{{ $main_schedule['away_name'] }}</p>
+                                <img src="{{ '/storage/logo/' . $main_schedule['away_team']['logo'] }}" alt=""
+                                    width="80px">
                             </div>
                             <div class="d-flex justify-content-start align-items-center gap-5 mt-3">
                                 <div class="d-flex align-items-center gap-3">
-                                    <i class="far fa-calendar"></i>
-                                    <p class="text-white">{{ $time[0] }}</p>
-                                </div>
-                                <div class="d-flex align-items-center gap-3">
                                     <i class="far fa-clock"></i>
-                                    <p class="text-white"> {{ $time[1] . ' WIB' }}</p>
+                                    <p class="text-white"> {{ $kickoff }}</p>
                                 </div>
                             </div>
                             <div class="d-flex align-items-center gap-3 mt-3">
                                 <i class="fas fa-map-marker-alt"></i>
                                 <p>{{ $main_schedule['location'] }}</p>
                             </div>
-                            <div id="newest-schedule-club"></div>
+                            <div class="newest-schedule-clubname">
+                                <p class="fw-bold fs-4 mb-3">{{ $main_schedule['home_team']['name'] }}</p>
+                                <p class="fw-bold fs-4">
+                                    &nbsp;&nbsp;&nbsp;{{ $main_schedule['away_team']['name'] }}</p>
+                            </div>
                         </div>
                     </div>
                 @else
@@ -50,41 +50,54 @@
                 @endif
 
                 <h1 class="fw-bold fs-3 my-3">Other Matches</h1>
-                @if ($schedule != null && $schedule->count() > 0)
-                    @foreach ($schedule as $index => $match)
+                @if ($schedule != null)
+                    @foreach ($schedule as $index => $value)
                         @php
-                            $utcTime = $match['date'] . ' ' . $match['time'];
-                            $localTime = Carbon\Carbon::parse($utcTime)->setTimezone('Asia/Jakarta');
-                            $time = explode(' ', $localTime);
+                            $kickoff = Carbon\Carbon::parse($value['kickoff'])->format('Y-m-d H:i');
                         @endphp
-                        <div class="match-box"
+                        <div class="match-box mb-4 p-3 rounded shadow"
                             style="background: linear-gradient(to left, rgba(0, 75, 117,  {{ 1 - $index * 0.05 }}), rgba(52, 61, 135,  {{ 1 - $index * 0.05 }}));">
-                            <div>
-                                <div class="club-match-box">
-                                    <div class="item-box">
-                                        <p class="fw-bold fs-4">{{ $match['home_name'] }}</p>
+                            <div class="match">
+                                <div class="card-responsive">
+                                    <p class="fw-bold fs-4 text-white m-0">{{ $value['home_team']['name'] }}</p>
+                                    <p class="fw-bold fs-4 text-white m-0">{{ $value['away_team']['name'] }}</p>
+                                    <div class="d-flex gap-2 align-items-center">
+                                        <i class="far fa-clock"></i>
+                                        <p class="text-white m-0">{{ $kickoff }}</p>
                                     </div>
-                                    <div>
-                                        <p>VS</p>
+                                </div>
+
+                                <div class="team-container">
+                                    <div class="team-name">
+                                        <p class="fw-bold fs-4 text-white m-0">{{ $value['home_team']['name'] }}</p>
                                     </div>
-                                    <div class="item-box">
-                                        <p class="fw-bold fs-4">{{ $match['away_name'] }}</p>
+                                    <img src="{{ asset('storage/logo/' . $value['home_team']['logo']) }}"
+                                        alt="{{ $value['home_team']['name'] }} logo" class="img-fluid team-logo">
+                                </div>
+                                <p class="text-white fs-3 fw-bold">VS</p>
+                                <div class="team-container">
+                                    <img src="{{ asset('storage/logo/' . $value['away_team']['logo']) }}"
+                                        alt="{{ $value['away_team']['name'] }} logo" class="img-fluid team-logo">
+                                    <div class="team-name">
+                                        <p class="fw-bold fs-4 text-white m-0 p-0">{{ $value['away_team']['name'] }}</p>
                                     </div>
                                 </div>
                             </div>
-                            <div class="item-box">
+                            <div class="kickoff">
                                 <i class="far fa-clock"></i>
-                                <p>{{ $localTime . ' WIB' }}</p>
+                                <p class="m-0">{{ $kickoff . ' WIB' }}</p>
                             </div>
-                            <div class="item-box">
-                                <i class="fas fa-map-marker-alt"></i>
-                                <p>{{ $match['location'] }}</p>
+                            <div class="d-flex align-items-center gap-2 text-white">
+                                <div class="location">
+                                    <i class="fas fa-map-marker-alt"></i>
+                                    <p class="m-0">{{ $value['location'] }}</p>
+                                </div>
                             </div>
                         </div>
                     @endforeach
 
-                    <div class="d-flex justify-content-center mt-4">
-                        {{ $schedule->links('pagination::bootstrap-4') }}
+                    <div class="d-flex justify-content-center my-4">
+                        {{ $fixtures->links('pagination::bootstrap-4') }}
                     </div>
                 @else
                     <div class="d-flex justify-content-center align-items-center w-100 p-2" id="no-schedule">
