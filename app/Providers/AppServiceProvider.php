8df410a63bb3369e1d0ef0e6fda80d13aa78cab2
unknown <kemalcrisannnaufal@gmail.com>
2024-06-13 20:30:23 +0700

    Fix link

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind('path.public', function() {
+            return realpath(base_path().'/../public_html');
+        });
     }
 
     /**
